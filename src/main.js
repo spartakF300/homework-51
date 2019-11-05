35912cf607cf4e550b6ef94af5514041d8deeadc
Spartak Fomenko <spartakfomenko28@gmail.com>
2019-11-06 00:31:59 +0600

    task1

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+const Card = (props)=> (
+    <div className="card">
+
+        <h3><span className="title_name">Название: </span>{props.name}</h3>
+
+        <p>Год: {props.age}</p>
+        <img src={props.img} alt=""/>
+    </div>
+);
+export default Card
+//
